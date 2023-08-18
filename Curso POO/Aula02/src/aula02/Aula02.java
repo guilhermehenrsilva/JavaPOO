912f1e14cd6492f42019b063fba3dd1d9e8fee14
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2023-08-18 20:16:07 -0300

    Atualizado 18/08/2023

---
+++ Curso POO/Aula02/src/aula02/Aula02.java
@@ -0,0 +1,27 @@
+
+package aula02;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Caneta  c1 = new Caneta();
+        c1.cor = " Azul";
+       // c1.ponta= 0.5f ;
+        c1.destampar();
+        c1.status();
+        c1.rabicar();
+        
+        
+        Caneta c2 = new Caneta();
+        
+        c2.modelo = "BIC Cristal";
+        c2.cor = "Azul";
+        c2.ponta = 0.5f ;
+        c2.carga = 80;
+        c2.tampada = false;
+        c2.rabicar();
+        
+        
+        
+    }
+    
+}
