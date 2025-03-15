9753341f5c496f4832d050bb7121567f854467e2
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2025-03-15 12:17:05 -0300

    Atualizado

---
+++ Curso POO/Aula07/src/aula07/Luta.java
@@ -0,0 +1,54 @@
+
+package aula07;
+
+
+public class Luta {
+    private String desafiado;
+    private String desafiante;
+    private int rounds;
+    private boolean aprovada;
+
+    
+    public Luta() {
+    }
+
+    public String getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(String desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public String getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(String desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean getAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+    
+    public void marcarLuta(){
+        
+    }
+   
+    public void lutar(){
+    }
+    
+}// fim da classe
