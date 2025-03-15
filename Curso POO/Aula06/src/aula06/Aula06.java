64203fd8065da991b516e618e3729a8eb4b3681b
Guilherme Henrique <guilherme_he_nrrique@hotmail.com>
2025-03-15 10:27:38 -0300

    Atualizado

--- Curso POO/Aula06/src/aula06/Aula06.java
+++ Curso POO/Aula06/src/aula06/Aula06.java
@@ -5,6 +5,20 @@ public class Aula06 {
 
     public static void main(String[] args) {
         
+        ControleRemoto c = new ControleRemoto();
+        
+        
+        c.ligar();
+        c.play();
+        c.maisVolume();
+        c.menosVolume();
+        c.menosVolume();
+        c.abrirMenu();
+        
+       
+        c.fecharMenu();
+        
+        
     }
     
 }
--- Curso POO/Aula06/src/aula06/ControleRemoto.java
+++ Curso POO/Aula06/src/aula06/ControleRemoto.java
@@ -15,27 +15,27 @@ public class ControleRemoto implements Controlador {
                 
     }
 
-    public int getVolume() {
+    private int getVolume() {
         return volume;
     }
 
-    public void setVolume(int volume) {
+    private void setVolume(int volume) {
         this.volume = volume;
     }
 
-    public boolean getLigado() {
+    private boolean getLigado() {
         return ligado;
     }
 
-    public void setLigado(boolean ligado) {
+    private void setLigado(boolean ligado) {
         this.ligado = ligado;
     }
 
-    public boolean getTocando() {
+    private boolean getTocando() {
         return tocando;
     }
 
-    public void setTocando(boolean Tocando) {
+    private void setTocando(boolean tocando) {
         this.tocando = tocando;
     }
     
@@ -53,41 +53,65 @@ public class ControleRemoto implements Controlador {
 
     @Override
     public void abrirMenu() {
+        System.out.println("-----MENU------");
         System.out.println("Está Ligado? " + this.getLigado());
         System.out.println("Está Tocando? " + this.getTocando());
         System.out.print("Volume: " + this.getVolume());
-        for (int i = 0 ;i <= this.getVolume(); i+=10){
-            System.out.print("[]");
+        for (int i = 0 ;i <= this.getVolume(); i+= 10){
+            System.out.print(" |");
         }
+        System.out.println("");
     }
 
     @Override
     public void fecharMenu() {
-        System.out.println("Fechando Menu ....");
+        System.out.println("Fechando Menu ...");
     }
 
     @Override
     public void maisVolume() {
+        if (this.getLigado()){
+            this.setVolume(this.getVolume() + 5);
+        } else {
+            System.out.println("Impossivel aumentar volume");
+        }
     }
 
     @Override
     public void menosVolume() {
+        if (this.getLigado()){
+            this.setVolume(this.getVolume() - 5);
+        } else {
+            System.out.println("Impossivel diminuir volume");
+        }
     }
 
     @Override
     public void ligarMudo() {
+        if (this.getLigado()&& this.getVolume()>0){
+        this.setVolume(0);
+        }
     }
 
     @Override
     public void desligarMudo() {
+        if(this.getLigado() && this.getVolume() == 0){
+        this.setVolume(50);
+        }
     }
 
     @Override
     public void play() {
+        if(this.getLigado()&& !(this.getTocando())){
+        this.setTocando(true);
+        }
     }
 
     @Override
     public void pause() {
+        if(this.getLigado() && (this.getTocando())){
+            this.setTocando(false);
+        }
     }
     
     
